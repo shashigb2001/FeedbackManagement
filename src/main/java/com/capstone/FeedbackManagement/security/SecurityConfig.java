230a4c0958ba9128efcbae81a11798730b33fe59
shashikumar <shashikumargowda657@gmail.com>
2025-09-10 21:28:23 +0530

    Updated Security filter chain

--- src/main/java/com/capstone/FeedbackManagement/security/SecurityConfig.java
+++ src/main/java/com/capstone/FeedbackManagement/security/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
+                .cors(cors -> {})
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(reg -> reg
                         .requestMatchers("/", "/index.html",
